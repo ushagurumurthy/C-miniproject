01350716ed415882edb59e8c4a2da346eb8578d0
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-15 15:27:28 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,15 @@
+#include "unity_internals.h"
+#include "unity.h"
+#include "header.h"
+void setUp(){}
+void tearDown(){}
+void testforpasswd(void){
+	TEST_ASSERT_EQUAL(0,check("potato"));
+
+}
+int main()
+{
+	UNITY_BEGIN();
+	RUN_TEST(testforpasswd);
+	return UNITY_END();
+}
