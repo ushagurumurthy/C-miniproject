04a8003694404f42d2cfdad18334edff11034849
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-16 13:00:13 +0530

    Add files via upload

---
+++ 3_Implementation/Test/test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include "../unity/unity.h"
+#include "../unity/unity_internals.h"
+void setUp(){}
+void tearDown(){}
+char confirm;
+void check(void)
+{
+    TEST_ASSERT_EQUAL('y',confirm);
+}
+/**
+ * @brief Check Whether any room is booked or not
+ * @brief Check File is empty or not
+ * @brief Using unit Test case
+ *
+ */
+int main(){
+    FILE *fp;
+	fp=fopen("rooms_reserved.txt","r");
+     if(fp == NULL)
+    {
+        confirm='n';
+    }else{
+        confirm='y';
+    }
+    UNITY_BEGIN();
+    RUN_TEST(check);
+    return UNITY_END();
+}
